a1e648a1b6a2be53bd2cca6c619a2a9a14b163f0
brent <brent@4be4153c-cb45-4615-a51f-74384bbadf62>
2014-03-10 05:26:04 +0000

    - Changed main function based on StackOverflow answer to avoid console spam in Xcode 5.
    git-svn-id: file:///Users/brent/Dropbox/Development/Repositories/ZenSudoku/trunk@217 4be4153c-cb45-4615-a51f-74384bbadf62

--- ZenSudoku/main.m
+++ ZenSudoku/main.m
@@ -1,3 +1,6 @@
+/*
+// Commenting out the normal main function to fix a bug in XCode 5 causing lots of console noise.
+ 
 //
 //  main.m
 //  ZenSudoku
@@ -12,6 +15,33 @@
 
 int main(int argc, char *argv[])
 {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZSAppDelegate class]));
+	}
+}
+*/
+
+#import <UIKit/UIKit.h>
+
+#import "ZSAppDelegate.h"
+
+
+typedef int (*PYStdWriter)(void *, const char *, int);
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size);
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+	if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
+		return 0;
+	}
+	return _oldStdWrite(inFD, buffer, size);
+}
+
+int main(int argc, char * argv[])
+{
+	_oldStdWrite = stderr->_write;
+	stderr->_write = __pyStderrWrite;
 	@autoreleasepool {
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZSAppDelegate class]));
 	}
